6f37e6f566c28102c378fdd7e0cf7e2d02aa7f1f
chenqi <chenqi@live.hk>
2022-12-08 19:56:07 +0800

    BM16 删除有序链表中重复的元素-II

---
+++ src/main/java/org/maimai/linkedlist/DeleteDuplicates2.java
@@ -0,0 +1,46 @@
+package org.maimai.linkedlist;
+
+import org.maimai.base.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * BM16 删除有序链表中重复的元素-II
+ * {1,2,2}
+ * {1}
+ *
+ * @Date 2022/12/8 19:16
+ * @Author chenqi
+ */
+public class DeleteDuplicates2 {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode p = head;
+        List<Integer> list = new ArrayList<>();
+        List<Integer> contains = new ArrayList<>();
+        while (p != null) {
+            int val = p.val;
+            if (list.contains(val)) {
+                contains.add(val);
+                p = p.next;
+                continue;
+            }
+            list.add(val);
+            p = p.next;
+        }
+        list.removeAll(contains);
+        ListNode dummy = new ListNode(-1);
+        ListNode dp = dummy;
+        if (!list.isEmpty()) {
+            for (Integer val : list) {
+                ListNode listNode = new ListNode(val);
+                dp.next = listNode;
+                dp = dp.next;
+            }
+        }
+        return dummy.next;
+    }
+
+
+}
