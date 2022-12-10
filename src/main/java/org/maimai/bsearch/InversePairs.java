991ce92fa754ceac2f2630d368a2e3f8ab7bc5f3
chenqi <chenqi@live.hk>
2022-12-10 10:00:13 +0800

    BM18 二维数组中的查找 BM19 寻找峰值

---
+++ src/main/java/org/maimai/bsearch/InversePairs.java
@@ -0,0 +1,23 @@
+package org.maimai.bsearch;
+
+/**
+ * [1,2,3,4,5,6,7,0]
+ * 7
+ * 逆序对的总数P P对1000000007取模
+ *
+ * @Date 2022/12/9 17:05
+ * @Author chenqi
+ */
+public class InversePairs {
+
+    public int inversePairs(int[] array) {
+
+
+
+
+
+
+
+        return 0;
+    }
+}
