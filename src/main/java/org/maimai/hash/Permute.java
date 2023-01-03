74639aeaabc2066e9a19589e188b4a1af8cd06d3
chenqi <chenqi@live.hk>
2023-01-03 23:32:50 +0800

    BM55 没有重复项数字的全排列

--- src/main/java/org/maimai/hash/Permute.java
+++ src/main/java/org/maimai/hash/Permute.java
@@ -34,17 +34,29 @@ public class Permute {
     }
 
     private void get(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> list, int index) {
+        // 分支进入结尾，找到一种排列
         if (index == list.size() - 1) {
             res.add(list);
         } else {
+            // 遍历后续的元素
             for (int i = index; i < list.size(); i++) {
+                // 交换二者
                 swap(list, i, index);
+                // 继续往后找
                 get(res, list, index + 1);
+                // 回溯
                 swap(list, i, index);
             }
         }
     }
 
+    /**
+     * 交换位置
+     *
+     * @param list
+     * @param a
+     * @param b
+     */
     private void swap(ArrayList<Integer> list, int a, int b) {
         int temp = list.get(a);
         list.set(a, list.get(b));
