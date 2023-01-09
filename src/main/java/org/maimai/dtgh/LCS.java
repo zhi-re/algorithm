b702157c56c2140df6f9493de607773e953ed883
chenqi <chenqi@live.hk>
2023-01-09 21:30:28 +0800

    BM66 最长公共子串

---
+++ src/main/java/org/maimai/dtgh/LCS.java
@@ -0,0 +1,49 @@
+package org.maimai.dtgh;
+
+/**
+ * BM66 最长公共子串
+ * <p>
+ * 给定两个字符串str1和str2，输出两个字符串的最长公共子序列。如果最长公共子序列为空，则返回"-1"。目前给出的数据，仅仅会存在一个最长的公共子序列
+ * <p>
+ * 输入：
+ * "1A2C3D4B56","B1D23A456A"
+ * 返回值：
+ * "123456"
+ *
+ * @Date 2023/1/9 19:44
+ * @Author chenqi
+ */
+public class LCS {
+    /**
+     * step 1：我们可以用dp[i][j]表示在str1中以第i个字符结尾在str2中以第j个字符结尾时的公共子串长度，
+     * step 2：遍历两个字符串填充dp数组，转移方程为：如果遍历到的该位两个字符相等，则此时长度等于两个前一位长度+1，
+     * dp[i][j]=dp[i−1][j−1]+1，如果遍历到该位时两个字符不相等，则置为0，因为这是子串，必须连续相等，断开要重新开始。
+     * step 3：每次更新dp[i][j]后，我们维护最大值，并更新该子串结束位置。
+     * step 4：最后根据最大值结束位置即可截取出子串。
+     *
+     * @param str1
+     * @param str2
+     * @return
+     */
+    public String LCS(String str1, String str2) {
+        int dp[][] = new int[str1.length() + 1][str2.length() + 1];
+        int max = 0;
+        int index = 0;
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+                if (dp[i][j] > max) {
+                    max = dp[i][j];
+                    index = i - 1;
+                }
+            }
+        }
+        return str1.substring(index - max + 1, index + 1);
+    }
+
+
+}
